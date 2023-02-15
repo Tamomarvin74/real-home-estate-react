6bcb5233ccb998f77c296a362e768527e0333fe7
Tamo Marvin <tamomarvin74@gmail.com>
2023-02-15 16:30:39 +0100

    create a header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+ import { useLocation,useNavigate} from "react-router"
+export default function Header() {
+    const location = useLocation();
+    const navigate = useNavigate()
+    function pathMatchRoute(route) {
+        if (route === location.pathname) {
+          return true;
+        }
+      }
+  return (
+    <div className="bg-white border-b shadow-sm sticky top-0 z-50"> 
+        <header className="flex justify-between items-center px-3   mx-auto">
+            <div>
+               <img src="https://realhomes.vn/wp-content/uploads/2019/11/cropped-Untitled.png" alt="logo" className="h-20 cursor-pointer" onClick={()=>navigate("/")} />
+            </div>
+            <div>
+                <ul className="flex space-x-10">
+                    <li className={` cursor-pointer py-3 font-semibold  text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/") && "text-black  border-b border-b-amber-600"}`} onClick={()=>navigate("/")}>Home</li>
+                    <li className={`cursor-pointer py-3 font-semibold  text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/Offers") && "text-black  border-b border-b-amber-600"}`} onClick={()=>navigate("/Offers")}>Offers</li>
+                    <li className={`cursor-pointer py-3 font-semibold  text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/SignIn") && "text-black  border-b border-b-amber-600"}`} onClick={()=>navigate("/SignIn")}>Sign in</li>
+                </ul>
+            </div>
+        </header>
+    </div>
+  )
+}
+
