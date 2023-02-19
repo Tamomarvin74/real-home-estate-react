bacc141f88cca310a0a630be4b113a10389d82c4
Tamo Marvin <tamomarvin74@gmail.com>
2023-02-19 18:54:37 +0100

    Add map to the listing page using leaflet and react-leaflet

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,79 +1,94 @@
 import { doc, getDoc } from "firebase/firestore";
-import { useEffect, useState } from "react"
+import { useState } from "react";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import { db } from "../firebase";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, {
-    EffectFade,
-    Autoplay,
-    Navigation,
-    Pagination,
-  } from "swiper";
-  import "swiper/css/bundle";
-  import {
-    FaShare,
-    FaMapMarkerAlt,
-    FaBed,
-    FaBath,
-    FaParking,
-    FaChair,
-  } from "react-icons/fa";
-  import { getAuth } from "firebase/auth";
+  EffectFade,
+  Autoplay,
+  Navigation,
+  Pagination,
+} from "swiper";
+import "swiper/css/bundle";
+import {
+  FaShare,
+  FaMapMarkerAlt,
+  FaBed,
+  FaBath,
+  FaParking,
+  FaChair,
+} from "react-icons/fa";
+import { getAuth } from "firebase/auth";
 import Contact from "../components/Contact";
-export default function Listing() {
-    const auth = getAuth();
-   const params = useParams();
-   const [listing, setlisting]= useState(null);
-   const [loading, setloading] = useState(true);
-   const [shareLinkCopied, setShareLinkCopied] = useState(false);
-   const [contactLandlord, setContactLandlord] = useState(false);
-   SwiperCore.use([Autoplay, Navigation, Pagination]);
-    useEffect(()=>{
-      async function fetchListing(){
-       const docRef = doc(db, "listings", params.listingId)
-       const docSnap = await getDoc(docRef)
-       if(docSnap.exists()){
-         setlisting(docSnap.data())
-         setloading(false)
-       
-       }
-      }
-      fetchListing(); 
-    },[params.listingId]);
-    if(loading){
-        return <Spinner/>;
-    }
-  return ( 
-  <main>
-    <Swiper slidesPerView={1} navigation pagination={{type:"progressbar"}} effect="fade" modules={[EffectFade]} Autoplay={{delay:3000 }}>
-        {listing.imgUrls.map((url, index)=>(
-            <SwiperSlide key={index}>
-               <div className="relative w-full overflow-hidden h-[300px]" style={{background: `url(${listing.imgUrls[index]}) center no-repeat`, backgroundSize:"cover"}}>
-                
-               </div>
-            </SwiperSlide>
-        ))}
-    </Swiper>
-    <div className="fixed mt-6 top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center" onClick={()=>{
-        navigator.clipboard.writeText(window.location.href);
-        setShareLinkCopied(true);
-        setTimeout(()=>{
-        setShareLinkCopied(false);
-        }, 2000)
-    }}
-    >
-        <FaShare className="text-lg text-slate-500" />
-    </div>
-    {shareLinkCopied &&(
-         <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">
-         Link Copied
-       </p>
-    )}
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
-    <div className=" m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded lg shadow-lg bg-white lg:space-x-5 ">
-        <div className=" w-full h-[200px] lg-[400px]">
-        <p className="text-2xl font-bold mb-3 text-blue-900">
+export default function Listing() {
+  const auth = getAuth();
+  const params = useParams();
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
+  const [contactLandlord, setContactLandlord] = useState(false);
+  SwiperCore.use([Autoplay, Navigation, Pagination]);
+  useEffect(() => {
+    async function fetchListing() {
+      const docRef = doc(db, "listings", params.listingId);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setListing(docSnap.data());
+        setLoading(false);
+      }
+    }
+    fetchListing();
+  }, [params.listingId]);
+  if (loading) {
+    return <Spinner />;
+  }
+  return (
+    <main>
+      <Swiper
+        slidesPerView={1}
+        navigation
+        pagination={{ type: "progressbar" }}
+        effect="fade"
+        modules={[EffectFade]}
+        autoplay={{ delay: 3000 }}
+      >
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              className="relative w-full overflow-hidden h-[300px]"
+              style={{
+                background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                backgroundSize: "cover",
+              }}
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div
+        className="mt-6 fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <FaShare className="text-lg text-slate-500" />
+      </div>
+      {shareLinkCopied && (
+        <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">
+          Link Copied
+        </p>
+      )}
+
+      <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5">
+        <div className=" w-full ">
+          <p className="text-2xl font-bold mb-3 text-blue-900">
             {listing.name} - ${" "}
             {listing.offer
               ? listing.discountedPrice
@@ -89,7 +104,7 @@ export default function Listing() {
             {listing.address}
           </p>
           <div className="flex justify-start items-center space-x-4 w-[75%]">
-          <p className="bg-red-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md">
+            <p className="bg-red-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md">
               {listing.type === "rent" ? "Rent" : "Sale"}
             </p>
             {listing.offer && (
@@ -103,7 +118,7 @@ export default function Listing() {
             {listing.description}
           </p>
           <ul className="flex items-center space-x-2 sm:space-x-10 text-sm font-semibold mb-6">
-          <li className="flex items-center whitespace-nowrap">
+            <li className="flex items-center whitespace-nowrap">
               <FaBed className="text-lg mr-1" />
               {+listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
             </li>
@@ -120,22 +135,41 @@ export default function Listing() {
               {listing.furnished ? "Furnished" : "Not furnished"}
             </li>
           </ul>
-          {listing.userRef !== auth.currentUser?.uid && !contactLandlord &&(
+          {listing.userRef !== auth.currentUser?.uid && !contactLandlord && (
             <div className="mt-6">
-            <button
-               onClick={() => setContactLandlord(true)}
-              className="px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg w-full text-center transition duration-150 ease-in-out "
-            >
-              Contact Landlord
-            </button>
-          </div>
+              <button
+                onClick={() => setContactLandlord(true)}
+                className="px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg w-full text-center transition duration-150 ease-in-out "
+              >
+                Contact Landlord
+              </button>
+            </div>
           )}
-                {contactLandlord && (
+          {contactLandlord && (
             <Contact userRef={listing.userRef} listing={listing} />
           )}
         </div>
-        <div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden"></div>
-    </div>
-  </main>
-  )
+        <div className="w-full h-[200px] md:h-[400px] z-10 overflow-x-hidden mt-6 md:mt-0 md:ml-2">
+          <MapContainer
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ height: "100%", width: "100%" }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>
+                {listing.address}
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
+    </main>
+  );
 }
