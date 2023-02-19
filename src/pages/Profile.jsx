d248bf173119a9b5bee86a276237e47b59641e55
Tamo Marvin <tamomarvin74@gmail.com>
2023-02-19 10:49:09 +0100

    add edit and delete fucntionality to the my listing section

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -86,7 +86,7 @@ export default function Profile() {
         (listing) => listing.id !== listingID
       );
       setListings(updatedListings);
-      toast.success("Successfully deleted the listing");
+      toast.success("Successfully deleted your listing");
     }
   }
   function onEdit(listingID) {
